5ea9fba110301b061bd1352ddeda25ff15411544
Chen Mu <3331400783@qq.com>
2024-05-23 13:39:23 +0800

    code

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function load(){
+    var list=document.querySelector('.header-box-list')
+    list.innerHTML+=list.innerHTML //复制长度为两倍
+    let left=list.style.left; //为list的左距离
+    let timer;//为定时器
+    clearInterval(timer);
+    var length = list.children[0].width * list.childElementCount / 2;
+    timer=setInterval((env)=>{
+        left=left - 2;
+        if(left <= -length){ 
+            left=0;
+        }
+        list.style.left=left + 'px';
+    }, 10);
+};
+document.addEventListener('DOMContentLoaded',load);
---
+++ js/page.js
@@ -0,0 +1,38 @@
+function load(){
+    var elements = document.querySelectorAll('.table-content-text');
+    elements.forEach(function(element) {
+        var childElement = element.querySelector('h2');
+        var computedStyle = window.getComputedStyle(childElement); // 获取计算后的样式
+        var marginTop = parseInt(computedStyle.getPropertyValue('margin-top')); // 获取上边距
+        var marginBottom = parseInt(computedStyle.getPropertyValue('margin-bottom')); // 获取下边距
+        var dx = childElement.offsetHeight + marginTop + marginBottom;
+        element.style.bottom = dx + 'px';
+    });
+};
+
+function animotion() {
+    var textElements = document.querySelectorAll('.table-content-text');
+    var imgElements = document.querySelectorAll('.table-content-img img');
+    textElements.forEach(function(textElement, index) {
+        var init = parseInt(textElement.style.bottom);
+        var tar = parseInt(textElement.offsetHeight);
+        var curImg = imgElements[index];
+        textElement.style.height = init + 'px';
+        textElement.style.marginBottom = -init + 'px';
+        textElement.addEventListener('mouseover', function() {
+            curImg.style.transform = 'scale(1.1)';
+            textElement.style.bottom = tar + 'px';
+            textElement.style.height = tar + 'px';
+            textElement.style.marginBottom = -tar + 'px';
+        });
+        textElement.addEventListener('mouseout', function() {
+            textElement.style.marginBottom = -init + 'px';
+            textElement.style.height = init + 'px';
+            textElement.style.bottom = init + 'px';
+            curImg.style.transform = 'scale(1)';
+
+        });
+    });
+}
+document.addEventListener('DOMContentLoaded',load);
+document.addEventListener('DOMContentLoaded',animotion);
