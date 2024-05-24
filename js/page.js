58602db3e8e5d88db75235b12f5a15a0843dc84f
Chen Mu <3331400783@qq.com>
2024-05-24 10:03:34 +0800

    修改了边框细节

--- js/page.js
+++ js/page.js
@@ -11,12 +11,14 @@ function load(){
 };
 
 function animotion() {
+    var boxElements = document.querySelectorAll('.main-table-content');
     var textElements = document.querySelectorAll('.table-content-text');
     var imgElements = document.querySelectorAll('.table-content-img img');
     textElements.forEach(function(textElement, index) {
         var init = parseInt(textElement.style.bottom);
         var tar = parseInt(textElement.offsetHeight);
         var curImg = imgElements[index];
+        var curbox = boxElements[index];
         textElement.style.height = init + 'px';
         textElement.style.marginBottom = -init + 'px';
         textElement.addEventListener('mouseover', function() {
@@ -24,10 +26,10 @@ function animotion() {
             textElement.style.bottom = tar + 'px';
             textElement.style.height = tar + 'px';
             textElement.style.marginBottom = -tar + 'px';
-            textElement.style.boxShadow = '0px 0px 15px #ffffff';
+            curbox.style.boxShadow = '0px 0px 15px #ffffff';
         });
         textElement.addEventListener('mouseout', function() {
-            textElement.style.boxShadow = 'none'
+            curbox.style.boxShadow = 'none'
             textElement.style.marginBottom = -init + 'px';
             textElement.style.height = init + 'px';
             textElement.style.bottom = init + 'px';
